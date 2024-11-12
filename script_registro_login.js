68aa94b3529a4bdda1d72f44fe7b8486f1b74f74
JulianaSerranoC <julianamariaserranocampo@gmail.com>
2024-11-11 21:06:04 -0500

    1Change11/11/24

---
+++ script_registro_login.js
@@ -0,0 +1,3 @@
+let formulario_login = document.querySelector(".formulario_login");
+let formulario_register = document.querySelector(".formulario_register");
+let contenedor_login_register = document.querySelector(".contenedor_login-register");
