f67ed96c518ac68dd20f77e3518a070d25ed3524
JulianaSerranoC <julianamariaserranocampo@gmail.com>
2024-11-13 22:59:38 -0500

    3Change13/11/24

--- js/script_login.js
+++ js/script_login.js
@@ -1,15 +1,16 @@
 document.getElementById("formulario_login").addEventListener("submit", function (event) {
-    event.preventDefault();
 
     const correoLogin = document.getElementById("correoLogin").value; 
     const contraseñaLogin = document.getElementById("contraseñaLogin").value;
 
     const usuarioRegistrado = JSON.parse(localStorage.getItem("usuarioRegistrado"));
 
-    if (usuarioRegistrado && usuarioRegistrado.correo === correoLogin && 
+    if (usuarioRegistrado.correo === correoLogin && 
         usuarioRegistrado.contraseña === contraseñaLogin) {
+
             localStorage.setItem("nombresUsuario", usuarioRegistrado.nombres);
             window.location.href = "saludo_index.html";
+
         } else {
             alert("Correo o contraseña incorrectos");
         }
